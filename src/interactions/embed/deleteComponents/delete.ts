51a44c70bdf89ac305494324de44d6a2dde9cbd1
Masha <97138114+cosymasha@users.noreply.github.com>
2024-01-24 05:11:57 +0100

    Traduction

--- src/interactions/embed/deleteComponents/delete.ts
+++ src/interactions/embed/deleteComponents/delete.ts
@@ -7,27 +7,27 @@ const select = new SelectMenu(
     if (!interaction.inCachedGuild() || !interaction.channel) return;
 
     if (!interaction.guild.members.me?.permissions.has(PermissionFlagsBits.ManageWebhooks))
-      return interaction.reply({ content: '`❌` この機能を使用するにはBOTに`ウェブフックの管理`権限を付与する必要があります。', ephemeral: true });
+      return interaction.reply({ content: '`❌` Pour utiliser cette fonctionnalité, le BOT doit avoir la permission `Gérer les webhooks`.', ephemeral: true });
 
     const targetId = interaction.message.embeds[0].footer?.text.match(/[0-9]{18,19}/)?.[0];
     const targetMessage = await (await interaction.channel.fetch()).messages.fetch(targetId!).catch(() => undefined);
 
     if (!targetMessage)
-      return interaction.reply({ content: '`❌` メッセージの取得中に問題が発生しました。', ephemeral: true });
+      return interaction.reply({ content: '`❌` Un problème est survenu lors de la récupération du message.', ephemeral: true });
 
     const webhook = await targetMessage.fetchWebhook().catch(() => null);
     if (!webhook || interaction.client.user.id !== webhook.owner?.id)
-      return interaction.reply({ content: '`❌` このメッセージは更新できません。', ephemeral: true });
+      return interaction.reply({ content: '`❌` Ce message ne peut pas être mis à jour.', ephemeral: true });
 
-    await interaction.update({ content: '`⌛` コンポーネントを削除中...', embeds: [], components: [] });
+    await interaction.update({ content: '`⌛` Suppression du composant en cours...', embeds: [], components: [] });
 
     const deleteIndex = interaction.values;
     const components = targetMessage.components;
 
     webhook
       .editMessage(targetMessage, { components: components.filter((v, index) => !deleteIndex.includes(String(index))) })
-      .then(() => interaction.editReply('`✅` コンポーネントを削除しました。'))
-      .catch(() => interaction.editReply('`❌` コンポーネントの削除に失敗しました。'));
+      .then(() => interaction.editReply('`✅` Composant supprimé avec succès.'))
+      .catch(() => interaction.editReply('`❌` Échec de la suppression du composant.'));
   },
 );
 
