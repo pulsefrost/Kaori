a834ec8a2de6035bc457eecd4af6ad00ee26467d
Masha <97138114+cosymasha@users.noreply.github.com>
2024-01-24 05:22:36 +0100

    Traduction

--- src/interactions/embed/embed/edit.ts
+++ src/interactions/embed/embed/edit.ts
@@ -7,28 +7,28 @@ const editEmbedButton = new Button(
     if (!interaction.inCachedGuild() || !interaction.channel) return;
 
     if (!interaction.guild.members.me?.permissions.has(PermissionFlagsBits.ManageWebhooks))
-      return interaction.reply({ content: '`❌` この機能を使用するにはBOTに`ウェブフックの管理`権限を付与する必要があります。', ephemeral: true });
+      return interaction.reply({ content: '`❌` Vous devez accorder à votre BOT l\'autorisation `Gérer les webhooks` pour utiliser cette fonctionnalité.', ephemeral: true });
 
     const targetId = interaction.message.content.match(/[0-9]{18,19}/)?.[0];
     const targetMessage = await (await interaction.channel.fetch()).messages.fetch(targetId || '').catch(() => undefined);
     if (!targetMessage)
-      return interaction.reply({ content: '`❌` メッセージの取得中に問題が発生しました。', ephemeral: true });
+      return interaction.reply({ content: '`❌` Un problème est survenu lors de la récupération du message.', ephemeral: true });
 
     const webhook = await targetMessage.fetchWebhook().catch(() => null);
     if (!webhook || interaction.client.user.id !== webhook.owner?.id)
-      return interaction.reply({ content: '`❌` このメッセージは更新できません。', ephemeral: true });
+      return interaction.reply({ content: '`❌` Vous ne pouvez pas modifier ce message.', ephemeral: true });
 
     const embeds = interaction.message.embeds;
     const components = interaction.message.components;
-    await interaction.update({ content: '`⌛` 埋め込みを編集中...', embeds: [], components: [] });
+    await interaction.update({ content: '`⌛` Édition de l\'intégration en cours...', embeds: [], components: [] });
     await webhook.edit({ channel: interaction.channelId });
 
     webhook
       .editMessage(targetMessage, { embeds })
-      .then(() => interaction.editReply('`✅` 埋め込みを編集しました！'))
+      .then(() => interaction.editReply('`✅` Intégration éditée avec succès !'))
       .catch(() => {
         interaction.editReply({ content: null, embeds, components });
-        interaction.followUp({ content: '`❌` 埋め込みの編集に失敗しました。', ephemeral: true });
+        interaction.followUp({ content: '`❌` Échec de l\'édition de l\'intégration.', ephemeral: true });
       });
   },
 );
