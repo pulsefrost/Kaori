6247509931a8d0561ab39bafc376d08facbb4a11
Masha <97138114+cosymasha@users.noreply.github.com>
2024-01-24 04:10:50 +0100

    Traduction

--- src/interactions/pauseInvite.ts
+++ src/interactions/pauseInvite.ts
@@ -3,37 +3,37 @@ import { ApplicationCommandOptionType, EmbedBuilder, GuildFeature, PermissionFla
 
 export default new ChatInput({
   name: 'pauseinvite',
-  description: 'サーバー招待の一時停止状態を切り替えます',
+  description: 'Basculer l\'état de pause des invitations du serveur',
   options: [
-    { name: 'pause', description: '一時停止状態にするか', type: ApplicationCommandOptionType.Boolean, required: true },
+    { name: 'pause', description: 'Activer ou désactiver l\'état de pause', type: ApplicationCommandOptionType.Boolean, required: true },
   ],
   dmPermission: false,
   defaultMemberPermissions: PermissionFlagsBits.Administrator,
 }, interaction => {
   const pause = interaction.options.getBoolean('pause', true);
-  if (!interaction.guild) return interaction.reply({ content: 'サーバー内でのみ使用できます', ephemeral: true });
+  if (!interaction.guild) return interaction.reply({ content: 'Utilisable uniquement à l\'intérieur du serveur', ephemeral: true });
   const guildFeatures = interaction.guild.features;
   if (guildFeatures.includes(GuildFeature.InvitesDisabled) === pause)
     return interaction.reply({
       embeds: [
         new EmbedBuilder()
-          .setDescription('❌ 既にその状態に設定されています')
+          .setDescription('❌ Déjà configuré dans cet état')
           .setColor('Red'),
       ], ephemeral: true,
     });
 
   if (pause)
-    interaction.guild.edit({ features: [...guildFeatures, GuildFeature.InvitesDisabled], reason: `招待一時停止 by ${interaction.user.tag}` }).then(() => {
+    interaction.guild.edit({ features: [...guildFeatures, GuildFeature.InvitesDisabled], reason: `Invitations en pause par ${interaction.user.tag}` }).then(() => {
       const embed = new EmbedBuilder()
-        .setDescription('✅ サーバー招待を**一時停止**しました！')
+        .setDescription('✅ Les invitations du serveur sont maintenant en **pause**!')
         .setColor('Green');
       return interaction.reply({ embeds: [embed], ephemeral: true });
     });
 
   else
-    interaction.guild.edit({ features: guildFeatures.filter(v => v !== GuildFeature.InvitesDisabled), reason: `招待一時停止解除 by ${interaction.user.tag}` }).then(() => {
+    interaction.guild.edit({ features: guildFeatures.filter(v => v !== GuildFeature.InvitesDisabled), reason: `Annulation de la pause des invitations par ${interaction.user.tag}` }).then(() => {
       const embed = new EmbedBuilder()
-        .setDescription('✅ サーバー招待の一時停止を**解除**しました！')
+        .setDescription('✅ La pause des invitations du serveur a été **levée**!')
         .setColor('Green');
       return interaction.reply({ embeds: [embed], ephemeral: true });
     });
