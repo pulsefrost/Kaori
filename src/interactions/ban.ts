ba1e634a3737d8821a61e023e10ad83edc1d8eaf
Kaori Café <dev@kaoricafe.fr>
2024-06-09 20:32:21 +0200

    Ajoûte du code

---
+++ src/interactions/ban.ts
@@ -0,0 +1,65 @@
+import { ApplicationCommandOptionType, codeBlock, Colors, EmbedBuilder, PermissionFlagsBits } from 'discord.js';
+import { ChatInput } from '@akki256/discord-interaction';
+
+const banCommand = new ChatInput(
+  {
+    name: 'ban',
+    description: 'Bannir un utilisateur du serveur',
+    options: [
+      {
+        name: 'user',
+        description: 'Utilisateur',
+        type: ApplicationCommandOptionType.User,
+        required: true,
+      },
+      {
+        name: 'reason',
+        description: 'Raison',
+        type: ApplicationCommandOptionType.String,
+      },
+    ],
+    defaultMemberPermissions: PermissionFlagsBits.BanMembers,
+    dmPermission: false,
+  },
+  { coolTime: 5000 },
+  (interaction) => {
+
+    if (!interaction.inCachedGuild()) return;
+
+    const member = interaction.options.getMember('user');
+
+    if (!(member instanceof GuildMember))
+      return interaction.reply({ content: '`❌` Cet utilisateur n\'est pas présent sur le serveur.', ephemeral: true });
+    if (member.id === interaction.user.id)
+      return interaction.reply({ content: '`❌` Vous ne pouvez pas vous bannir vous-même.', ephemeral: true });
+    if (!member.bannable)
+      return interaction.reply({ content: '`❌` Échec du ban en raison de permissions insuffisantes.', ephemeral: true });
+    if (!interaction.member.permissions.has(PermissionFlagsBits.BanMembers) && interaction.member.roles.highest.position <= member.roles.highest.position)
+      return interaction.reply({ content: '`❌` Vous n\'avez pas les permissions nécessaires pour bannir cet utilisateur.', ephemeral: true });
+
+    member.ban({ reason: interaction.options.getString('reason') ?? 'Aucune raison fournie' })
+      .then(() => {
+        interaction.reply({
+          embeds: [
+            new EmbedBuilder()
+              .setDescription(`\`✅\` ${member} a été banni du serveur.`)
+              .setColor(Colors.Green),
+          ],
+          ephemeral: true,
+        });
+      })
+      .catch((err) => {
+        interaction.reply({
+          embeds: [
+            new EmbedBuilder()
+              .setDescription(`\`❌\` Échec du ban.\n${codeBlock(err)}`)
+              .setColor(Colors.Red),
+          ],
+          ephemeral: true,
+        });
+      });
+
+  },
+);
+
+module.exports = [banCommand];
