1c67985cdfb38a0503d4aa9289a53cfbfddc0912
frostslack <elhad@kimcafe.fr>
2024-09-08 16:07:03 +0200

    Update help.ts

--- src/interactions/help.ts
+++ src/interactions/help.ts
@@ -1,16 +1,17 @@
-
+import { ChatInputCommandInteraction } from 'discord.js';
 import { ChatInput } from '@akki256/discord-interaction';
 import { EmbedBuilder } from 'discord.js';
 import fs from 'fs';
 import path from 'path';
 
+// Commande Help
 const helpCommand = new ChatInput(
   {
     name: 'help',
     description: 'Affiche la liste des commandes disponibles et leurs descriptions',
     dmPermission: true,
   },
-  async (interaction) => {
+  async (interaction: ChatInputCommandInteraction) => {
     // Path to the interactions (commands) folder
     const commandsPath = path.join(__dirname, 'interactions');
 
@@ -18,12 +19,12 @@ const helpCommand = new ChatInput(
     const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts') || file.endsWith('.js'));
 
     // Array to hold command details
-    let commandList = [];
+    let commandList: string[] = [];
 
     // Loop through each command file and extract name and description
     for (const file of commandFiles) {
-      const command = require(path.join(commandsPath, file));
-
+      const command = await import(path.join(commandsPath, file));
+      
       // Check if the command has a name and description
       if (command.default?.name && command.default?.description) {
         commandList.push(`**/${command.default.name}** - ${command.default.description}`);
